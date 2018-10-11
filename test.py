03a31fba3a14d0369026738f31b39591db802510
maonn <melvinmaonn@qq.com>
2018-10-11 15:43:24 +0800

    1.改为预测100条道路1min的速度，并且适应了新的metrics

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ from sklearn import preprocessing
 from sklearn.externals import joblib
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-from metrics import MAPE
+import metrics
+import datetime
 
-ROAD_NUM = 655
+ROAD_NUM = 100
 
 STEP = 12
 
-PRED_TIME = 4
+PRED_TIME = 1
 
 TIME_LENGTH = 1021*30 - STEP - PRED_TIME + 1
 
@@ -32,17 +33,23 @@ time_seq = np.append(time_seq,np.arange(1021 - STEP - PRED_TIME + 1))
 
 for index, road_velocity in enumerate(data):
 
+    if index % 6 != 0 or index == 0:
+        continue
+
+    if index / 6 > 100:
+        break
+
     X_temp = np.zeros(shape=(TIME_LENGTH, STEP))
 
     for i in range(0, road_velocity.size - STEP - PRED_TIME + 1):
         X_temp[i] = np.array(road_velocity[i:i + STEP])
-        y[index*TIME_LENGTH + i] = road_velocity[i + STEP + PRED_TIME - 1]
+        y[(int)(index / 6 - 1) * TIME_LENGTH + i] = road_velocity[i + STEP + PRED_TIME - 1]
 
     id_seq = np.full((TIME_LENGTH,), road_id[index])
     id_time = np.vstack((id_seq, time_seq))
     id_time = id_time.T
     result = np.hstack((id_time, X_temp))
-    X[index*TIME_LENGTH : (index + 1)*TIME_LENGTH] = result
+    X[(int)(index / 6 - 1) * TIME_LENGTH:(int)(index / 6) * TIME_LENGTH] = result
 
 print(X.size)
 print(y.size)
@@ -56,13 +63,20 @@ y_test = y[21*1021:]
 
 
 # 径向基核函数初始化的SVR
-rbf_svr = joblib.load('save/rbf_svr_4min.pkl')
-rbf_svr_y_predict = rbf_svr.predict(X_test)
+rbf_svr = joblib.load('save/rbf_svr_100_1min_test_2.pkl')
 
-np.savetxt('prediction_655r_4min_test.txt',rbf_svr_y_predict)
+start = datetime.datetime.now()
+rbf_svr_y_predict = rbf_svr.predict(X_test)
+end = datetime.datetime.now()
+print("预测耗时：" + str(end - start))
+
+np.savetxt('prediction_100r_1min_test_test_2.txt', rbf_svr_y_predict)
 
 print(' ')
-print('R-squared value of RBF SVR is',rbf_svr.score(X_test,y_test))
-print('The root mean squared error of RBF SVR is',np.sqrt(mean_squared_error(y_test,rbf_svr_y_predict)))
-print('The mean absolute error of RBF SVR is',mean_absolute_error(y_test,rbf_svr_y_predict))
-print('The mean absolute percentage error of RBF SVR is',MAPE(y_test,rbf_svr_y_predict))
+start = datetime.datetime.now()
+print('R-squared value of RBF SVR is', rbf_svr.score(X_test, y_test))
+print('The root mean squared error of RBF SVR is', metrics.masked_rmse_np(rbf_svr_y_predict, y_test, 0))
+print('The mean absolute error of RBF SVR is', metrics.masked_mae_np(rbf_svr_y_predict, y_test, 0))
+print('The mean absolute percentage error of RBF SVR is', metrics.masked_mape_np(rbf_svr_y_predict, y_test, 0))
+end = datetime.datetime.now()
+print("计算metrics耗时：" + str(end - start))
